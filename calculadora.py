456376a59544cf77878e0458f543ad872b02c025
AlvesLuis0 <alves.luis.3002@gmail.com>
2022-10-25 21:03:26 -0300

    Mudança de lógica

--- calculadora.py
+++ calculadora.py
@@ -7,132 +7,76 @@ import math
 root = Tk()
 root.title("Calculadora Científica")
 root.resizable(False, False)
-root.geometry("210x300")
+root.geometry("180x300")
 
 
-#criando função que seleciona o operador
+#criando função para adicionar algarismos
+def addNumber(label: Label, alg):
+    global atual_number
+
+    if alg != "." or "." not in atual_number:
+        atual_number += alg
+        label.config(text=atual_number)
+        label.grid(row=0, column=0)
+
+#criando a borracha
+def backspace(label: Label):
+    global atual_number
+    atual_number = atual_number[:-1]
+    label.config(text=atual_number)
+
+
+#criando função para criar botões mais facilmente
+def button(text, vector, command=lambda: 0, bg="blue"):
+    Button(
+        root,
+        text=text,
+        bg=bg,
+        fg="white",
+        width=5,
+        height=2,
+        command=command
+    ).grid(row=vector[0], column=vector[1])
+
+
+#renderizando o "visor" da calculadora
 operator = ""
+equation = ""
+label1 = Label(root, text=equation)
+label1.grid(row=0, column=0)
 
-def select(oper):
-    global operator
-    operator = oper
-
-
-#criando função que calcula
-def calc():
-    global operator
-
-    operators_functions = {
-        "+": lambda: n1.get() + n2.get(),
-        "-": lambda:  n1.get() - n2.get(),
-        "*": lambda:  n1.get() * n2.get(),
-        "/": lambda:  n1.get() / n2.get(),
-        "√": lambda: n1.get() ** 0.5,
-        "^": lambda: n1.get() ** n2.get(),
-        "sen": lambda: math.sin(n1.get()),
-        "cos": lambda: math.cos(n1.get()),
-        "tan": lambda: math.tan(n1.get()),
-        "": ""
-    }
-
-    result.config(text=operators_functions[operator]())
-    result.pack()
+atual_number = ""
+label2 = Label(root, text=atual_number)
+label2.grid(row=1, column=0)
 
 
-#renderizando input de N1
-n1 = Entry(root).grid(row=0,column=0)
+#renderizando o "botões" da calculadora
+button("BACKSPACE", [1,1], lambda: backspace(label2))
 
+button("sen", [2,0])
+button("cos", [2,1])
+button("tan", [2,2])
+button("/", [2,3])
 
-#renderizando operadores
-Button(
-    root,
-    text="+",
-    bg="blue",
-    fg="white",
-    command=lambda: select("+")
-).grid(row=1,column=0)
+button(7, [3,0], lambda: addNumber(label2, "7"))
+button(8, [3,1], lambda: addNumber(label2, "8"))
+button(9, [3,2], lambda: addNumber(label2, "9"))
+button("*", [3,3])
 
-Button(
-    root,
-    text="-",
-    bg="blue",
-    fg="white",
-    command=lambda: select("-")
-).grid(row=1,column=1)
+button(4, [4,0], lambda: addNumber(label2, "4"))
+button(5, [4,1], lambda: addNumber(label2, "5"))
+button(6, [4,2], lambda: addNumber(label2, "6"))
+button("-", [4,3])
 
-Button(
-    root,
-    text="*",
-    bg="blue",
-    fg="white",
-    command=lambda: select("*")
-).grid(row=1,column=2)
+button(1, [5,0], lambda: addNumber(label2, "1"))
+button(2, [5,1], lambda: addNumber(label2, "2"))
+button(3, [5,2], lambda: addNumber(label2, "3"))
+button("+", [5,3])
 
-Button(
-    root,
-    text="/",
-    bg="blue",
-    fg="white",
-    command=lambda: select("/")
-).grid(row=2,column=0)
-
-Button(
-    root,
-    text="√",
-    bg="blue",
-    fg="white",
-    command=lambda: select("√")
-).grid(row=2,column=1)
-
-Button(
-    root,
-    text="^",
-    bg="blue",
-    fg="white",
-    command=lambda: select("^")
-).grid(row=2,column=2)
-
-Button(
-    root,
-    text="sen",
-    bg="blue",
-    fg="white",
-    command=lambda: select("sen")
-).grid(row=3,column=0)
-
-Button(
-    root,
-    text="cos",
-    bg="blue",
-    fg="white",
-    command=lambda: select("cos")
-).grid(row=3,column=1)
-
-Button(
-    root,
-    text="tan",
-    bg="blue",
-    fg="white",
-    command=lambda: select("tan")
-).grid(row=3,column=2)
-
-
-#renderizando input de N2
-n2 = Entry(root).grid(row=4,column=0)
-
-
-#renderizando local de resposta
-result = Label(root)
-
-
-#renderizando botão para calcular
-Button(
-    root,
-    text="Calcular",
-    bg="blue",
-    fg="white",
-    command=lambda: calc()
-).grid(row=5,column=0)
+button("√", [6,0])
+button(0, [6,1], lambda: addNumber(label2, "0"))
+button(".", [6,2], lambda: addNumber(label2, "."))
+button("=", [6,3])
 
 
 #iniciando looping
